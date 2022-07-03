2b62f0e2911366ce85689803fd89842e368e3892
emediong <emediong5135@gmail.com>
2022-07-03 12:46:04 +0100

    configured environment variables for email host user and password

--- config/settings.py
+++ config/settings.py
@@ -153,7 +153,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 DEFAULT_FROM_EMAIL = 'emediong5135@gmail.com' 
 EMAIL_HOST = 'smtp.gmail.com' 
-EMAIL_HOST_USER = 'emediong5135@gmail.com' 
-EMAIL_HOST_PASSWORD = 'yduimzzvefjcvlhi' 
+EMAIL_HOST_USER = env.str('EMAIL_HOST_USER') 
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587 
 EMAIL_USE_TLS = True
